72fb94cabbb191ff187d8239963628debe6239b7
thegrabes08 <graber.melanie@gmail.com>
2016-09-23 23:10:42 -0400

    added front.page.php to child theme

--- wp-content/themes/accelerate-theme-child/front-page copy.php
+++ wp-content/themes/accelerate-theme-child/front-page copy.php
@@ -13,7 +13,9 @@
  */
 
 get_header(); ?>
-
+<pre>
+	<?php print_r($wp_query); exit; ?>
+</pre>
 <section class="home-page">
 	<div class="site-content">
 		<?php while ( have_posts() ) : the_post(); ?>
@@ -24,5 +26,20 @@ get_header(); ?>
 		<?php endwhile; // end of the loop. ?>
 	</div><!-- .container -->
 </section><!-- .home-page -->
+
+<section class="recent-posts">
+	 <div class="site-content">
+		  <div class="blog-post">
+			   <h4>From the Blog</h4>
+			    <?php query_posts('posts_per_page=1'); ?>
+				     <?php while ( have_posts() ) : the_post(); ?>
+				       <h2><?php the_title(); ?></h2>
+				       <?php the_excerpt(); ?>
+				       <a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
+					<?php endwhile; ?> 
+			    <?php wp_reset_query(); ?>
+		   </div>
+	  </div>
+</section>
 
 <?php get_footer(); ?>
