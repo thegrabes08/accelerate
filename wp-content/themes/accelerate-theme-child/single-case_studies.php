7e66e9fcebc96c6d20f007f307d50ea33d02c3e9
thegrabes08 <graber.melanie@gmail.com>
2016-09-27 19:53:33 -0400

    added custom fields plugin and template tags to single-case_studies file so I can avoid hardcoding everything in the admin

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -16,10 +16,42 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
+			<!-- create all of the variables -->
+			<?php while ( have_posts() ) : the_post();
+				$services = get_field('services');
+				$clients = get_field('clients');
+				$link = get_field('site_link');
+				$image_1 = get_field('image_1');
+				$image_2 = get_field('image_2');
+				$image_3 = get_field('image_3');
+				$size = "full"; //used for images
+			?>
+				<!-- use template tags and custom tags to create structure -->
                 <article class="case-study">
-                    <?php the_content(); ?>
+					<aside class = "case-study-sidebar">
+						<h2><?php the_title(); ?></h2>
+						<h6><?php echo $services; ?></h6>
+						<h5>Client: <?php echo $clients; ?></h5>
+
+	                    <?php the_content(); ?>
+
+						<p><a href="<?php echo $link; ?>">Site Link</a></p>
+					</aside>
+
+					<div class = "case-study-images">
+					<!-- wrap images in if/else statements so it is ok to leave one out -->
+						<?php if($image_1) { ?>
+							<?php echo wp_get_attachment_image( $image_1, $size ); ?>
+						<?php } ?>
+						<?php if($image_2) { ?>
+							<?php echo wp_get_attachment_image( $image_2, $size ); ?>
+						<?php  } ?>
+						<?php if($image_1) { ?>
+							<?php echo wp_get_attachment_image( $image_3, $size ); ?>
+						<?php  } ?>
+					</div>
                 </article>
+
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
